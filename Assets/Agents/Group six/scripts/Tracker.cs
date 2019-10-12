6547a09b04e29799c0a0f6e20b7ae7fd49f0295a
shervpey <43882892+shervpey@users.noreply.github.com>
2019-10-11 18:06:15 -0700

    Group Six

---
+++ Assets/Agents/Group six/scripts/Tracker.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    public bool run = false;
+    public GameObject Target;
+    public float smoothspeed = 0.125f;
+    private Vector3 newPosition = Vector3.zero;
+    // float example=0;
+    float cameraY = 60f;
+    float minY = 10;
+    float maxY = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        newPosition = Camera.main.transform.position;
+        //Debug.Log("addition example: " + CoolAddition(2, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.mouseScrollDelta != Vector2.zero)
+        {
+            Debug.Log("scroll; " + Input.mouseScrollDelta);
+            cameraY = Mathf.Clamp(cameraY + Input.mouseScrollDelta.y*-5, minY, maxY);
+        }
+        // smooth move camera
+        Vector3 cameraPosition = Camera.main.transform.position;
+        newPosition.y = cameraY;
+        Vector3 smoothedPosition = Vector3.Lerp(cameraPosition, newPosition, smoothspeed);
+        Camera.main.transform.position = smoothedPosition;
+
+        Vector3 clickPosition = Vector3.zero;
+
+        //Left mouse botton click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("left click");
+            Vector3 hit = GetClickHit("ground");
+            if (hit !=Vector3.zero)
+            {
+                Target.transform.position = hit;
+            }
+            //Debug.Log("screenPoint: " + screenPoint);
+           // Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+            ///Debug.DrawRay(ray.origin, ray.direction * 100, Color.red,1);
+
+        }
+        //Right click mouse botton click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("right click");
+
+            Vector3 hit = GetClickHit("ground");
+            if (hit !=Vector3.zero)
+            {
+                newPosition = hit;
+            }
+          
+        }
+        Vector3 GetClickHit(string tag)
+        {
+            //create a ray from the camera
+
+            Vector3 screenPoint = Input.mousePosition;
+           // Debug.Log("screenPoint: " + screenPoint);
+            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray.origin, ray.direction, Mathf.Infinity);
+            foreach (RaycastHit hit in hits)
+            {
+                Debug.Log("layer: " + LayerMask.NameToLayer(tag));
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer(tag))
+                {
+                    return hit.point;
+                }
+            }
+            return Vector3.zero;
+        }
+        //Space click
+    }
+}
---
+++ Assets/Scripts/SampleAgent.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class sampleagent : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    public GameObject[] targets;
+    public float changeTargetDistance = 3;
+    int t;
+    public bool shuffleTargets = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //grab targets using tags
+        if (targets == null || targets.Length == 0)
+            if (shuffleTargets)
+            {
+                targets = Shuffle(targets);
+            }
+        //Debug.Log(this.name + "has " + targets.Length + "Target");
+
+        agent = GetComponent<NavMeshAgent>();
+        t = 0;
+        target = targets[t].transform;
+        agent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeTargetDistance > Vector3.Distance(agent.transform.position, target.position))
+        {
+            t++;
+            if (t == targets.Length)
+            {
+                t = 0;
+            }
+            Debug.Log(this.name + "Change Target: " + t);
+            target = targets[t].transform;
+            agent.SetDestination(target.position); // each frame set the agent's destination to the
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            Debug.Log("TRAM STOP");
+            agent.isStopped = true;
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            Debug.Log("TRAM START");
+            agent.isStopped = false;
+        }
+
+
+    }
+
+    GameObject[] Shuffle(GameObject[] objects)
+    {
+        GameObject tempGO;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            Debug.Log("i: " + i);
+            int rnd = Random.Range(0, targets.Length);
+            tempGO = targets[rnd];
+            targets[rnd] = targets[i];
+            targets[i] = tempGO;
+        }
+        return objects;
+
+
+    }
+
+    
+
+    
+
+}
