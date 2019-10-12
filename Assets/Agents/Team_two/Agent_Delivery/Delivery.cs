8c7489e76a293a956073b75f5eab088eb752ba21
yachia1 <55905078+yachia1@users.noreply.github.com>
2019-10-11 22:01:40 -0700

    team 2 agent behavior added

--- Assets/Agents/Team_two/Agent_Delivery/Delivery.cs
+++ Assets/Agents/Team_two/Agent_Delivery/Delivery.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SampleAgent : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     Transform target;
     NavMeshAgent agent;
