faa3127a78bb8642fc18e9845ef88de78f8d9d1e
yachia1 <55905078+yachia1@users.noreply.github.com>
2019-10-28 22:18:30 -0700

    Team2 Drone first perspective

---
+++ Assets/Agents/Team_two/Agent_Drone V2/droneControl1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droneControl1 : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public GameObject drone;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public float distance = 30f;
+    public float lerpTime = 5;
+    private float currentlerpTime = 0;
+    private bool keyHit = false;
+
+    public bool getheight = false;
+
+    //private bool isHeigh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+
+        startPos = drone.transform.position;
+        endPos = drone.transform.position + Vector3.up * distance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            keyHit = true;
+        }
+
+        if (keyHit == true)
+        {
+            currentlerpTime += Time.deltaTime;
+            if (currentlerpTime >= lerpTime)
+            {
+                currentlerpTime = lerpTime;
+            }
+            float perc = currentlerpTime / lerpTime;
+            drone.transform.position = Vector3.Lerp(startPos, endPos, perc);
+
+        }
+
+        if (drone.transform.position.y >= distance)
+        {
+            getheight = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            keyHit = true;
+        }
+
+        
+
+    }
+
+}
