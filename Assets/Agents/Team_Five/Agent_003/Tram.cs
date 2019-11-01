f0475acd978598b9b8cf9a1baa4215b8e3bbe0e3
anatoligeorg <55858624+anatoligeorg@users.noreply.github.com>
2019-10-31 20:05:30 -0700

    tram_update1_script

--- Assets/Agents/Team_Five/Agent_003/Tram.cs
+++ Assets/Agents/Team_Five/Agent_003/Tram.cs
@@ -5,73 +5,145 @@ using UnityEngine.AI;
 
 public class Tram : MonoBehaviour
 {
-    Transform target;
+    #region GLOBAL VARIABLES
+    GameObject target;
     NavMeshAgent agent;
-    public GameObject[] targets;
+    public string[] targetNames;
+    public bool isRider = false;
+
+
+    public Vector3 position;
+
+    [Header("Target Info")]
     public float changeTargetDistance = 3;
-    int t;
-    public bool shuffleTargets = true;
+    private int t;
+    public bool shuffleTargets = false;
+    public GameObject[] targets;
+
+    [Header("Wait Times")]
+    public float waitTimeStop = 0;
+    public float waitTimeTarget = 0;
+
+    public float waitTime = 0;
+    private bool waiting = false;
+    private float waited = 0;
+
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        //grab targets using tags
-        if (targets == null || targets.Length == 0)
-            if (shuffleTargets)
-            {
-                targets = Shuffle(targets);
-            }
-        //Debug.Log(this.name + "has " + targets.Length + "Target");
+        gameObject.tag = "Tram";
 
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>(); //set the agent variable to this game object's navmesh
         t = 0;
-        target = targets[t].transform;
-        agent.SetDestination(target.position);
+        target = targets[t];
+        agent.SetDestination(target.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (changeTargetDistance > Vector3.Distance(agent.transform.position, target.position))
+
+        if (target.transform.position != position)
         {
-            t++;
-            if (t == targets.Length)
+            position = target.transform.position;
+            agent.SetDestination(position);
+
+        }
+
+        //original text if (!waiting) // (waiting == false) (1 == 0)
+        if (waiting) // (waiting == false) (1 == 0)
+        {
+            if (waited > waitTime)
             {
-                t = 0;
+                waiting = false;
+                agent.isStopped = false;
+                waited = 0;
             }
-            Debug.Log(this.name + "Change Target: " + t);
-            target = targets[t].transform;
-            agent.SetDestination(target.position); // each frame set the agent's destination to the
-        }
+            else
+            {
+                waited += Time.deltaTime;
+            }
 
-        if (Input.GetKeyDown("s"))
-        { 
-            Debug.Log("Tram Stop");
-            agent.isStopped = true;
-        }
-
-        if (Input.GetKeyDown("a"))
+        } //if waiting
+        else
         {
-            Debug.Log("Tram Start");
+            //see agent's next destination
+            Debug.DrawLine(transform.position, agent.steeringTarget, Color.black);
+
+            float distanceToTarget = Vector3.Distance(agent.transform.position, target.transform.position);
+            //change target once it is reached
+            if (changeTargetDistance > distanceToTarget) //have we reached our target
+            {
+                //type of stop
+                if (target.name.Contains("t_target"))
+                {
+                    waitTime = waitTimeTarget;
+                }
+
+                if (target.name.Contains("Stop"))
+                {
+                    waitTime = waitTimeStop;
+                }
+
+                t++;
+                if (t == targets.Length)
+                {
+                    t = 0;
+                }
+                Debug.Log(this.name + " Change Target: " + t);
+                target = targets[t];
+                agent.SetDestination(target.transform.position); //each frame set the agent's destination to the target position
+
+                waiting = true;
+                agent.isStopped = true;
+
+            } // changeTargetDistance test
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("collision: " + collision.gameObject.name);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pedestrian"))
+        {
+            agent.isStopped = true;
+            obstacles++; // obstacles = obstacles + 1; || obstacles += 1;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("exited");
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pedestrian"))
+        {
+            obstacles--; //obstacles = obstacles - 1; || obstacles -= 1;
+        }
+        if (obstacles == 0) //once there are zero obstacles, start the agent moving
+        {
             agent.isStopped = false;
         }
     }
 
+
+
+    private int obstacles = 0;
+
     GameObject[] Shuffle(GameObject[] objects)
     {
         GameObject tempGO;
-        for (int i = 0; i < targets.Length; i++)
+        for (int i = 0; i < objects.Length; i++)
         {
-            Debug.Log("i: " + i);
-            int rnd = Random.Range(0, targets.Length);
-            tempGO = targets[rnd];
-            targets[rnd] = targets[i];
-            targets[i] = tempGO;
+            //Debug.Log("i: " + i);
+            int rnd = Random.Range(0, objects.Length);
+            tempGO = objects[rnd];
+            objects[rnd] = objects[i];
+            objects[i] = tempGO;
+
         }
         return objects;
 
-
     }
-
-   
 }
