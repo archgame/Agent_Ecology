03e7bbfab18b45b811d6a6abfe63cdf41833ed9d
Jingyi-Huang-user <54973336+Jingyi-Huang-user@users.noreply.github.com>
2019-11-16 02:43:39 -0800

    Group four- intersection update

---
+++ Assets/Agents/Group four/Scripts/Intersection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intersection : MonoBehaviour
+{
+    [Header("Controls")]
+    public bool switchLights = false;
+
+    [Header("Lights")]
+    public GameObject[] TrafficLights;
+    public GameObject[] Crosswalks;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (switchLights)
+        {
+            SwitchLights(true, false, true, false);
+        }
+        else
+        {
+            SwitchLights(false, true, false, true);
+        }
+        
+    }
+
+    public void SwitchLights(bool L1, bool L2, bool L3, bool L4)
+    {
+        TrafficLights[0].SetActive(L1);
+        TrafficLights[1].SetActive(L2);
+        TrafficLights[2].SetActive(L3);
+        TrafficLights[3].SetActive(L4);
+
+        Crosswalks[0].SetActive(!L1);
+        Crosswalks[1].SetActive(!L2);
+        Crosswalks[2].SetActive(!L3);
+        Crosswalks[3].SetActive(!L4);
+        Crosswalks[4].SetActive(!L1);
+        Crosswalks[5].SetActive(!L2);
+        Crosswalks[6].SetActive(!L3);
+        Crosswalks[7].SetActive(!L4);
+    }
+}
---
+++ Assets/Agents/Group four/Scripts/IntersectionStop.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IntersectionStop : MonoBehaviour
+{
+   
+
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pedestrian"))
+        {
+            GameObject passenger = collision.gameObject;
+
+
+            passenger.GetComponent<NavMeshAgent>().speed = 0;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("SmallVehicle"))
+        {
+            GameObject passenger = collision.gameObject;
+            passenger.GetComponent<NavMeshAgent>().speed = 0;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("LargeVehicle"))
+        {
+            GameObject passenger = collision.gameObject;
+            passenger.GetComponent<NavMeshAgent>().speed = 0;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pedestrian"))
+        {
+            GameObject passenger = collision.gameObject;
+            passenger.GetComponent<NavMeshAgent>().speed = 1.5f;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("SmallVehicle"))
+        {
+            GameObject passenger = collision.gameObject;
+            passenger.GetComponent<NavMeshAgent>().speed = 6;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("LargeVehicle"))
+        {
+            GameObject passenger = collision.gameObject;
+            passenger.GetComponent<NavMeshAgent>().speed = 15;
+            Debug.Log("exit triggered: " + passenger.name);
+        }
+    }
+
+
+    /*void OnTriggerStay(Collider collision)
+    {
+        GameObject passenger = collision.gameObject;
+        passenger.GetComponent<NavMeshAgent>().isStopped = true;
+    }*/
+}
