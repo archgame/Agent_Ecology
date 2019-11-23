63f6c15c6e18cb6399a79961139f869e4ec7e834
aroadtoyou <55912133+aroadtoyou@users.noreply.github.com>
2019-11-22 20:50:18 -0800

    DogScripts added
    For the Guide dog and Dogwalker in a wheel chair.

---
+++ Assets/Agents/Team_Five/Agent_001/DogGuideDog/WCDrawLine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WCDrawLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private LineRenderer lineRender;
+    private float counter;
+    private float dist;
+
+    public Transform origin;
+    public Transform destination;
+
+    public float lineDrawSpeed = 6f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        lineRender = GetComponent<LineRenderer>();
+        lineRender.SetPosition(0, origin.position);
+        lineRender.SetWidth(0.1f, 0.1f);
+
+
+
+        dist = Vector3.Distance(origin.position, destination.position);
+
+        if (counter < dist)
+        {
+            counter += .1f / lineDrawSpeed;
+            float x = Mathf.Lerp(0, dist, counter);
+            Vector3 pointA = origin.position;
+            Vector3 pointB = destination.position;
+
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRender.SetPosition(1, pointAlongLine);
+        }
+    }
+}
