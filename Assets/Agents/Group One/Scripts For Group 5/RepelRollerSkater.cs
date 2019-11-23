6ac6ff0a68e209cfb5aae82da94755b5e4458110
anatoligeorg <55858624+anatoligeorg@users.noreply.github.com>
2019-11-23 11:00:59 -0800

    ClassChanges

--- Assets/Agents/Group One/Scripts For Group 5/RepelRollerSkater.cs
+++ Assets/Agents/Group One/Scripts For Group 5/RepelRollerSkater.cs
@@ -8,14 +8,14 @@ public class RepelRollerSkater : MonoBehaviour
     void OnTriggerStay(Collider col)
     {
         //guard statement
-        if(col.gameObject.tag != "RollerSkates") { return; }
+        if(col.gameObject.tag != "RollerSkates") { return; } // Check to see if schoolchild
         Debug.Log("Stay: " + col.gameObject.name);
-    
-        NavMeshAgent agent = col.gameObject.GetComponent<NavMeshAgent>();
+   
+        NavMeshAgent agent = col.gameObject.GetComponent<NavMeshAgent>(); //do we need this? No agent data comes from game object not collider
         if(agent == null) { return;}
 
         float speed = agent.velocity.magnitude * Time.deltaTime;
-        Vector3 direction = col.gameObject.transform.position - transform.position;
+        Vector3 direction = col.gameObject.transform.position - transform.position; //flip this
 
         Vector3 velocity = direction * speed * 0.4f;
         float angle = Vector3.Angle(velocity, agent.velocity);
