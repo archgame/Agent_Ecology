5c28f75ae8b7018156fb7a8808fbaa2d2556c62b
anatoligeorg <55858624+anatoligeorg@users.noreply.github.com>
2019-11-30 21:30:21 -0800

    cnean up scrippts

--- Assets/Agents/Team_Five/Agent_003/TramScripts/NoNavmesh/PickUpPassenger.cs
+++ Assets/Agents/Team_Five/Agent_003/TramScripts/NoNavmesh/PickUpPassenger.cs
@@ -3,38 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PUpDoff : MonoBehaviour
+public class PickUpPassenger : MonoBehaviour
 {
     public GameObject tram;
     public List<int> passengerStops = new List<int>();
-    public float PassengerSpeed = 0;
+    public float PassengerSpeed = 0;    
     public GameObject Tramtarget;
     public GameObject Stoptarget;
-
-
+    
+    
 
 
 
     // Start is called before the first frame update
     void Start()
-    {
-
-
+    {        
+       
+       
     }
 
-
+    
     void Update()
     {
 
 
-        if (tram.GetComponent<TramGo>().MyPath[0].Reached == true)
+        if (bus.GetComponent<TramGo>().MyPath[0].Reached == true)
         {
-            gameObject.transform.position = Vector3.MoveTowards(transform.position, Tramtarget.transform.position, PassengerSpeed * Time.deltaTime);
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, Bustarget.transform.position, PassengerSpeed * Time.deltaTime);
             Debug.Log("went to bus");
 
-            if (tram.GetComponent<TramGo>().ReadyToGo == true)
+            if (bus.GetComponent<TramGo>().ReadyToGo == true)
             {
-                gameObject.transform.SetParent(Tramtarget.transform);
+                gameObject.transform.SetParent(Bustarget.transform);
                 gameObject.GetComponent<MeshRenderer>().enabled = false;
                 gameObject.GetComponent<NavMeshAgent>().enabled = false;
                 gameObject.GetComponent<Collider>().enabled = false;
@@ -44,21 +44,21 @@ public class PUpDoff : MonoBehaviour
         }
 
 
-        if (tram.GetComponent<TramGo>().MyPath[1].Reached == true)
+        if (bus.GetComponent<TramGo>().MyPath[1].Reached == true)
         {
             gameObject.transform.SetParent(null);
-            gameObject.GetComponent<PedPassenger>().enabled = true;
+            gameObject.GetComponent<Pedestrian>().enabled = true;
             gameObject.GetComponent<MeshRenderer>().enabled = true;
             gameObject.GetComponent<Collider>().enabled = true;
             gameObject.transform.position = Vector3.MoveTowards(transform.position, Stoptarget.transform.position, PassengerSpeed * Time.deltaTime);
-            gameObject.GetComponent<PUpDoff>().enabled = false;
-            gameObject.GetComponent<NavMeshAgent>().enabled = true;
+            gameObject.GetComponent<PickUpPassenger>().enabled = false;
+            gameObject.GetComponent<NavMeshAgent>().enabled = true;            
+                        
+        }      
 
-        }
-
-
-
+           
+               
     }
 
-
+  
 }
---
+++ Assets/Agents/Team_Five/Scripts/Intersection.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Intersection : MonoBehaviour
+{
+    [Header("Controls")]
+    public bool switchLights1 = false;
+    public bool switchLights2 = false;
+    public bool switchLights3 = false;
+    public bool switchLights4 = false;
+    public bool switchLightsPed = false;
+
+
+    [Header("Lights")]
+    public GameObject[] trafficLights;
+    public GameObject[] crosswalks;
+
+    [Header("Wait")]
+    public float waitTime = 0;
+    private bool waiting = false;
+    private float waited = 0;
+
+    void Start()
+    {
+        //InvokeRepeating(Update);
+        //StartCoroutine("Update");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (true)
+        {
+            if (switchLights1)
+            {
+                SwitchLights(false, true, true, true);
+                switchLightsPed = false;
+            }
+            else
+            {
+                SwitchLights(true, false, false, false);
+            }
+
+            if (switchLights2)
+            {
+                switchLights1 = false;
+                switchLightsPed = false;
+                SwitchLights(true, false, true, true);
+            }
+            else
+            {
+                //SwitchLights(false,true,false, false);
+            }
+
+            if (switchLights3)
+            {
+
+                switchLights1 = false;
+                switchLights2 = false;
+                switchLightsPed = false;
+                SwitchLights(true, true, false, true);
+            }
+            else
+            {
+                //SwitchLights(false,false,true,false);
+            }
+
+            if (switchLights4)
+            {
+                switchLights1 = false;
+                switchLights2 = false;
+                switchLights3 = false;
+                //switchLightsPed = false;
+                SwitchLights(true, true, true, false);
+            }
+            else
+            {
+                //SwitchLights(false,false,false,true);
+            }
+
+            if (switchLightsPed)
+            {
+                switchLights1 = false;
+                switchLights2 = false;
+                switchLights3 = false;
+                switchLights4 = false;
+                SwitchLights(true, true, true, true);
+
+                crosswalks[0].SetActive(false);
+                crosswalks[1].SetActive(false);
+                crosswalks[2].SetActive(false);
+                crosswalks[3].SetActive(false);
+            }
+            else
+            {
+                //SwitchLights(true,true,true,true);
+                crosswalks[0].SetActive(true);
+                crosswalks[1].SetActive(true);
+                crosswalks[2].SetActive(true);
+                crosswalks[3].SetActive(true);
+            }
+        }
+    }
+
+    public void SwitchLights(bool l1, bool l2, bool l3, bool l4)
+    {
+        trafficLights[0].SetActive(l1);
+        trafficLights[1].SetActive(l2);
+        trafficLights[2].SetActive(l3);
+        trafficLights[3].SetActive(l4);
+
+        /*crosswalks[0].SetActive(!l1);
+        crosswalks[1].SetActive(!l2);
+        crosswalks[2].SetActive(!l3);
+        crosswalks[3].SetActive(!l4);*/
+    }
+
+    
+}
